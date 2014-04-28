86b90a23c25b8a466cdb07f309fd5cccef07d063
Damian Gryski <damian@gryski.com>
2014-04-28 21:25:22 +0200

    docs++

--- farmhash.go
+++ farmhash.go
@@ -1,4 +1,11 @@
 // Package farmhash is cgo bindings against Google's FarmHash family of non-cryptographic hash functions
+/*
+
+https://code.google.com/p/farmhash/
+
+Note: This package does not yet support the 128-bit hash functions
+
+*/
 package farmhash
 
 /*
@@ -23,30 +30,37 @@ import "C"
 
 import "unsafe"
 
+// Hash32 hashes a byte slice and returns a uint32 hash value
 func Hash32(s []byte) uint32 {
 	return uint32(C.cHash32((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s))))
 }
 
+// Hash64 hashes a byte slice and returns a uint64 hash value
 func Hash64(s []byte) uint64 {
 	return uint64(C.cHash64((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s))))
 }
 
+// Hash32Seed hashes a byte slice and a uint32 seed and returns a uint32 hash value
 func Hash32Seed(s []byte, seed uint32) uint32 {
 	return uint32(C.cHash32Seed((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s)), C.uint32_t(seed)))
 }
 
+// Hash64Seed hashes a byte slice and a uint64 seed and returns a uint64 hash value
 func Hash64Seed(s []byte, seed uint64) uint64 {
 	return uint64(C.cHash64Seed((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s)), C.uint64_t(seed)))
 }
 
+// Hash64Seeds hashes a byte slice and two uint64 seeds and returns a uint64 hash value
 func Hash64Seeds(s []byte, seed0, seed1 uint64) uint64 {
 	return uint64(C.cHash64Seeds((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s)), C.uint64_t(seed0), C.uint64_t(seed1)))
 }
 
+// Fingerprint32 is a 32-bit fingerprint function for byte-slices
 func Fingerprint32(s []byte) uint32 {
 	return uint32(C.cFingerprint32((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s))))
 }
 
+// Fingerprint64 is a 64-bit fingerprint function for byte-slices
 func Fingerprint64(s []byte) uint64 {
 	return uint64(C.cFingerprint64((*C.char)(unsafe.Pointer(&s[0])), C.size_t(len(s))))
 }
